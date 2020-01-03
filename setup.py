fcc198194db9a3a83c57167260e71c35f022f016
olzeug <34414119+olzeug@users.noreply.github.com>
2020-01-03 12:11:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 __project__ = "get_chefkoch"
 __version__ = "0.0.3"
-__description__ = "A Python program with which you can get data from Chefkoch."
+__description__ = "A Python Library with which you can get data from Chefkoch."
 __packages__ = ["get_chefkoch"]
 __keywords__ = ["Chefkoch","python"]
 __requires__ = ["requests","feedparser","bs4","json","lxml"]
