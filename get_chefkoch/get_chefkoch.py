62043f909360b4889bd7430559986a719dd5a7fb
olzeug <olzeug@gmail.com>
2020-07-14 13:16:20 +0200

    Everything adapted to the latest changes from Chefkoch

--- get_chefkoch/get_chefkoch.py
+++ get_chefkoch/get_chefkoch.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 
 import requests,feedparser
 from bs4 import BeautifulSoup
-from json import loads
+import json
 class _SaveRecipe(object):
     def __init__(self):
         pass
@@ -14,31 +13,44 @@ class chefkoch:
     def daily_recipe(self):
         feed = feedparser.parse("https://www.chefkoch.de/rss/rezept-des-tages.php")
         url = feed['entries'][0]['link']
-        recipe = self.get(url.split("/")[4])
+        recipe = self._get(url.split("/")[4])
         return(recipe)
 
-    def get(self,recipe_id):
+    def _get(self,recipe_id):
         url = "https://www.chefkoch.de/rezepte/"+ str(recipe_id)
         recipe = _SaveRecipe()
-        content = BeautifulSoup(requests.get(url).text, 'lxml')
-        recibe_json = loads(content.findAll("script",type="application/ld+json")[1].text)
-        recipe.name = recibe_json["name"]
-        recipe.description = recibe_json["recipeInstructions"]
-        recipe.image = recibe_json["image"]
-        recipe.ingredients = recibe_json["recipeIngredient"]
-        recipe.rating = float(recibe_json["aggregateRating"]["ratingValue"])
-        recipe.category = recibe_json["recipeCategory"]
-        recipe.published = recibe_json["datePublished"]
-        recipe.cooktime = recibe_json["prepTime"]
-        recipe.autor = recibe_json["author"]["name"]
-        recipe.reviews = recibe_json["aggregateRating"]["reviewCount"]
-        recipe.Yield = recibe_json["recipeYield"]
+        content = BeautifulSoup(requests.get(url).text, 'html.parser')
+        recipe_json = json.loads(content.findAll("script",type="application/ld+json")[1].string)
+        for item in ["name", "recipeInstructions", "image", "recipeIngredient", "recipeCategory", "datePublished", "prepTime", "recipeYield", ["aggregateRating", "ratingValue"], ["aggregateRating", "reviewCount"]]:
+            if type(item) == str:
+              recipe_json.setdefault(item, None)
+            else:
+              recipe_json.setdefault(item[0], dict())
+              recipe_json[item[0]].setdefault(item[1], None)
+
+        recipe.name = recipe_json["name"]
+        recipe.description = recipe_json["recipeInstructions"]
+        recipe.image = recipe_json["image"]
+        recipe.ingredients = recipe_json["recipeIngredient"]
+        try:
+          recipe.rating = float(recipe_json["aggregateRating"]["ratingValue"])
+        except:
+          recipe.rating = recipe_json["aggregateRating"]["ratingValue"]
+        recipe.category = recipe_json["recipeCategory"]
+        recipe.published = recipe_json["datePublished"]
+        recipe.cooktime = recipe_json["prepTime"]
+        recipe.autor = recipe_json["author"]["name"]
+        recipe.reviews = recipe_json["aggregateRating"]["reviewCount"]
+        recipe.Yield = recipe_json["recipeYield"]
         recipe.id = recipe_id
-        recibe.calories = recibe_json["nutrition"]["calories"]
+        #recipe.calories = recipe_json["nutrition"]["calories"]
         return recipe
-    def search(self,search):
+
+    def search(self, search, limit=10, offset=0):
+        result = list()
         url = "https://www.chefkoch.de/rs/s0/"+search+"/Rezept.html"
-        content = BeautifulSoup(requests.get(url).text, 'lxml')
-        objects = loads(content.findAll('script', type='application/ld+json')[1].text)["itemListElement"]
-        ckid = objects[0]["url"].split("/")[4]
-        return(self.get(ckid))
+        content = BeautifulSoup(requests.get(url).text, 'html.parser')
+        objects = json.loads(content.findAll('script', type='application/ld+json')[1].string)["itemListElement"]
+        for recipe in objects[offset:limit]:
+            result += [self._get(recipe["url"].split("/")[4])]
+        return(result)
