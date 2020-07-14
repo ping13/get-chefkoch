9040486db1f9bec9a6575fe96c0712f5bc0e04bf
olzeug <olzeug@gmail.com>
2020-07-14 13:29:37 +0200

    Add url Support

--- get_chefkoch/get_chefkoch.py
+++ get_chefkoch/get_chefkoch.py
@@ -43,6 +43,7 @@ class chefkoch:
         recipe.reviews = recipe_json["aggregateRating"]["reviewCount"]
         recipe.Yield = recipe_json["recipeYield"]
         recipe.id = recipe_id
+        recipe.url = url
         #recipe.calories = recipe_json["nutrition"]["calories"]
         return recipe
 
