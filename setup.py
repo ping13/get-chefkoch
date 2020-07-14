4a5e1ba680ad21d119c336c0bcd9a82e453683a2
olzeug <olzeug@gmail.com>
2020-07-14 10:37:33 +0200

    Add README.md to pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup
 __project__ = "get_chefkoch"
-__version__ = "0.0.3"
+__version__ = "0.0.5"
 __description__ = "A Python Library with which you can get data from Chefkoch."
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 __packages__ = ["get_chefkoch"]
 __keywords__ = ["Chefkoch","python"]
 __requires__ = ["requests","feedparser","bs4","json","lxml"]
@@ -12,4 +16,8 @@ setup(
     packages = __packages__,
     keywords = __keywords__,
     requires = __requires__,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/olzeug/get_chefkoch",
+    python_requires='>=3.0',
 )
